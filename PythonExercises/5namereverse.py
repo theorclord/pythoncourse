508523e0078918e07716255947d3019d359c19e6
Mikkel Stolborg <mikkel.stolborg@capgemini.com>
2021-09-10 09:14:21 +0200

    changed names for excerises

---
+++ PythonExercises/5namereverse.py
@@ -0,0 +1,4 @@
+first = input("Enter first name: ")
+last = input("Enter last name: ")
+
+print("Hello", last, first)
---
+++ PythonExercises/8firstlast.py
@@ -0,0 +1,3 @@
+colorList = ["Red","Green","White","Black"]
+print(colorList)
+print(f"First {colorList[0]}",f"Last {colorList[-1]}",sep="\n")
